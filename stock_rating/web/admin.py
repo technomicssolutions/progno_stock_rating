3cc84db32b46faa1b2dddf29e6f13501ae47004d
technomics <remya@technomicssolutions.com>
2014-07-03 15:24:44 +0530

    db design, model creation, migrations, admin registration

---
+++ stock_rating/web/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from web.models import *
+
+
+
+admin.site.register(UserPermission)
+admin.site.register(AnalyticalHead)
+admin.site.register(DataField)
+admin.site.register(DataFile)
+admin.site.register(FieldMap)
+admin.site.register(FunctionCategory)
+admin.site.register(Operator)
+admin.site.register(Formula)
+admin.site.register(Function)
+admin.site.register(HardcodedFormula)
+admin.site.register(ContinuityFunction)
+admin.site.register(ConsistencyFunction)
+admin.site.register(Industry)
+admin.site.register(Company)
+admin.site.register(ScoreRating)
+admin.site.register(AnalysisModel)
+admin.site.register(ParameterLimit)
+admin.site.register(StarRating)
+admin.site.register(CompanyFunctionScore)
+admin.site.register(CompanyModelScore)
+
--- stock_rating/web/models.py
+++ stock_rating/web/models.py
@@ -1,3 +1,181 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Date(models.Model):
+
+    created_date = models.DateTimeField('Created Date', auto_now_add=True)
+    updated_date = models.DateTimeField('Updated Date', auto_now=True)
+
+class UserPermission(Date):
+
+    user = models.ForeignKey(User)
+    data_upload = models.BooleanField('Data Upload', default=False)
+    field_settings = models.BooleanField('Field Settings', default=False)
+    score_settings = models.BooleanField('Score Settings', default=False)
+    function_settings = models.BooleanField('Function Settings', default=False)
+    analytical_heads = models.BooleanField('Analytical heads', default=False)
+
+    def __unicode__(self):
+        return self.user.first_name
+
+class AnalyticalHead(Date):
+
+    created_by = models.ForeignKey(User)
+    title = models.CharField('Title', max_length=200)
+    description = models.TextField('Description', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+class DataField(Date):
+    created_by = models.ForeignKey(User)
+    name = models.CharField('Name', max_length=200)
+    description = models.TextField('Description', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Field'
+        verbose_name_plural = 'Fields'
+
+class DataFile(Date):
+
+    uploaded_by = models.ForeignKey(User)
+    uploaded_file = models.FileField(upload_to='uploads/data_file/')
+
+    def __unicode__(self):
+        return self.user.first_name + ' - ' + self.uploaded_file.name
+
+class FieldMap(models.Model):
+
+    data_file = models.ForeignKey(DataFile)
+    data_field = models.ForeignKey(DataField)
+    file_field = models.CharField('File Field', max_length=200, unique=True)
+
+    def __unicode__(self):
+        return str(self.data_file) + ' - ' + self.file_field
+
+class FunctionCategory(models.Model):
+
+    category_name = models.CharField('Category Name', max_length=200)
+    description = models.TextField('Description', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.category_name
+
+class Operator(models.Model):
+    symbol = models.CharField('Symbol', max_length=1)
+
+class Formula(models.Model):
+    operands = models.ManyToManyField(DataField)
+    operators = models.ManyToManyField(Operator)
+
+    def display(self):
+        pass
+    def __unicode__(self):
+        return self.display()
+
+FUNCTION_TYPES = (
+    ('general', 'General'),
+    ('continuity', 'Continuity'),
+    ('consistency', 'Consistency'),
+)
+
+class Function(Date):
+
+    category = models.ForeignKey(FunctionCategory)
+    analytical_head = models.ForeignKey(AnalyticalHead)
+    function_name = models.CharField('Name', max_length=200)
+    description = models.TextField('Description', null=True, blank=True)
+    function_type = models.CharField('Type', choices=FUNCTION_TYPES, max_length=11)
+    formula = models.ForeignKey(Formula, null=True, blank=True)
+
+    def __unicode__(self):
+        return function_name
+
+class HardcodedFormula(models.Model):
+    continuity_formula = models.CharField('Continuity Formula', max_length=200)
+    consistency_formula = models.CharField('Consistency Formula', max_length=200)
+
+class ContinuityFunction(Function):
+    number_of_periods = models.IntegerField('Number of Periods', max_length=3)
+    minimum_value = models.IntegerField('Minimum Value', max_length=3)
+    period_1 = models.IntegerField('Period 1', max_length=5)
+    period_2 = models.IntegerField('Period 2', max_length=5)
+    period_3 = models.IntegerField('Period 3', max_length=5)
+
+class ConsistencyFunction(Function):
+    number_of_periods = models.IntegerField('Number of Periods', max_length=3)
+    minimum_value = models.IntegerField('Minimum Value', max_length=3)
+    period_1 = models.IntegerField('Period 1', max_length=5)
+    mean = models.IntegerField('Mean', max_length=5)
+
+class Industry(models.Model):
+    industry_name = models.CharField('Industry', max_length=200, unique=True)
+
+    def __unicode__(self):
+        return industry_name
+
+class Company(models.Model):
+    company_name = models.CharField('company_name', max_length=200, unique=True)
+    isin_code = models.CharField('ISIN Code', max_length=200, unique=True)
+    industry = models.ForeignKey(Industry)
+
+    def __unicode__(self):
+        return self.company_name + ' - ' + self.isin_code
+
+class ScoreRating(Date):
+    strong_score = models.IntegerField('Strong', max_length=1, default=0)
+    neutral_score = models.IntegerField('Neutral', max_length=1, default=0)
+    week_score = models.IntegerField('Week', max_length=1, default=0)
+
+
+class AnalysisModel(Date):
+
+    name = models.CharField('Model name', max_length=100)
+    description = models.TextField('Description')
+    insdustries = models.ManyToManyField(Industry)
+    analytical_heads = models.ManyToManyField(AnalyticalHead)
+
+    def __unicode__(self):
+        return name
+
+class ParameterLimit(models.Model):
+
+    analysis_model = models.ForeignKey('AnalysisModel')
+    function = models.ForeignKey(Function)
+    strong_min = models.FloatField('Strong Min', max_length=5)
+    strong_max = models.FloatField('Strong Max', max_length=5)
+    neutral_min = models.FloatField('Neutral Min', max_length=5)
+    neutral_max = models.FloatField('Neutral Max', max_length=5)
+    week_min = models.FloatField('Week Min', max_length=5)
+    week_max = models.FloatField('Week Max', max_length=5)
+    strong_comment = models.CharField('Strong Comment', max_length=200)
+    neutral_comment = models.CharField('Neutral Comment', max_length=200)
+    week_comment = models.CharField('Week Comment', max_length=200)
+        
+class StarRating(models.Model):
+
+    star_count = models.IntegerField('StarCount', max_length=1)
+    min_score = models.FloatField('Min Score', max_length=5)
+    max_score = models.FloatField('Max Score', max_length=5)
+    comment = models.CharField('Comment', max_length=200)
+
+    def __unicode__(self):
+        return str(self.star_count) + ' star'
+
+class CompanyFunctionScore(models.Model):
+    company = models.ForeignKey(Company)
+    function = models.ForeignKey(Function)
+    score = models.FloatField('Function Score', max_length=5)
+
+class CompanyModelScore(models.Model):
+    company = models.ForeignKey(Company)
+    analysis_model = models.ForeignKey(AnalysisModel)
+    score = models.IntegerField('Model Score', max_length=5)
+    star_rating = models.ForeignKey(StarRating)
+
+    def __unicode__(self):
+        return self.company
