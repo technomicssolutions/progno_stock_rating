d05944538164a8b7b83c76cae462c1c74b834354
technomics <remya@technomicssolutions.com>
2014-10-23 14:34:11 +0530

    recaptcha login completed

--- stock_rating/public/urls.py
+++ stock_rating/public/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
 
-from views import (Login, Logout, Home, Signup, StarRating, StarRatingReport)
+from views import (Login, Logout, Home, Signup, StarRating, StarRatingReport, \
+		VerifyRecaptcha)
 
 LOGIN_URL = '/login/'
 
@@ -14,7 +15,8 @@ urlpatterns = patterns('',
     url(r'^logout/$', Logout.as_view(), name="public_logout"),
     url(r'^signup/$', Signup.as_view(), name="signup"),
     url(r'^star_rating/$', login_required(StarRating.as_view(), login_url="/login/"), name="star_rating"),
-    url(r'^star_rating_report/$', login_required(StarRatingReport.as_view(), login_url="/login/"), name="star_rating_report")
+    url(r'^star_rating_report/$', login_required(StarRatingReport.as_view(), login_url="/login/"), name="star_rating_report"),
+    url(r'^verify_recaptcha/$', VerifyRecaptcha.as_view(), name="verifiy_recaptcha")
 )
 
 
