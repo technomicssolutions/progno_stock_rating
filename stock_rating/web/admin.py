a64d6bcaac8feab5a989f6e7f8be82a74bd62fdb
technomics <remya@technomicssolutions.com>
2014-11-04 17:28:03 +0530

    admin changes

--- stock_rating/web/admin.py
+++ stock_rating/web/admin.py
@@ -20,7 +20,7 @@ class CompanyStockDataAdmin(admin.ModelAdmin):
 	search_fields = ['company__company_name']
 
 class CompanyModelFunctionPointAdmin(admin.ModelAdmin):
-	search_fields = ['company__company_name', 'parameter_limit__function__function_name', 'parameter_limit__model__name']
+	search_fields = ['company__company_name', 'parameter_limit__function__function_name', 'parameter_limit__analysis_model__name']
 
 class NSEBSEPriceAdmin(admin.ModelAdmin):
 	search_fields = ['company__company_name']
