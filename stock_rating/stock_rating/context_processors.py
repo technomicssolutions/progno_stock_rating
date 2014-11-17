ddfd0d863edbfaa11629c5f20d8660a60c700c44
technomics <remya@technomicssolutions.com>
2014-11-17 14:22:49 +0530

    context processor error corrected

--- stock_rating/stock_rating/context_processors.py
+++ stock_rating/stock_rating/context_processors.py
@@ -5,13 +5,15 @@ from public.models import PublicUser
 
 def site_variables(request):
 	user_permission = ''
-	if request.user:
+	try:		
 		if request.user.is_authenticated():	
 			if request.user.publicuser_set.all().count() == 0:	
 				if request.user.is_superuser:				
 					user_permission, created = UserPermission.objects.get_or_create(user=request.user, data_upload=True, field_settings=True, score_settings=True, function_settings=True, analytical_heads=True)	   		
 			   	else:
-			   		user_permission = UserPermission.objects.get(user = request.user)	   		
+			   		user_permission = UserPermission.objects.get(user = request.user)
+	except:
+		pass	   		
 	return {
 	 'user_permission': user_permission,
 	}
