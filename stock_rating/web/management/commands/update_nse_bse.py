9bce23c7f5f9c64d5385dc5a283feb5c91bca5e0
technomics <remya@technomicssolutions.com>
2015-01-15 11:43:41 +0530

    nsebse price update

--- stock_rating/web/management/commands/update_nse_bse.py
+++ stock_rating/web/management/commands/update_nse_bse.py
@@ -36,7 +36,7 @@ class Command(BaseCommand):
 
 def get_bse_price():
     date = datetime.now().date()
-    #date = date + timedelta(days=-1)
+    date = date + timedelta(days=-1)
     hdr = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
